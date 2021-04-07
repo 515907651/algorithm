f99d8a705017460d7001a0cf82e9e3ebb1105fcf
Gang <515907651@qq.com>
2021-04-07 10:38:28 +0800

    feat: 二分法插入目标值
    33题，easy，查找或者插入目标值

---
+++ src/Arrays/SearchInsert_35_easy.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class SearchInsert_35_easy {
+    public int searchInsert(int[] nums, int target) {
+        // 查找简单，插入怎么做呢
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // 这里结果是left == right，所以返回left和right是一样的
+        return left + 1;
+
+    }
+
+}
